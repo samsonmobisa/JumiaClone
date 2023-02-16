67547a4628627f27dde61005d74f91e23f0cdaa5
Samson Mobisa <samsonmobisa62@gmail.com>
2023-02-16 16:01:59 +0300

    Jumia clone website
    Front-end Jumia clone for HTML, CSS and Javascript.

---
+++ index.js
@@ -0,0 +1,11 @@
+// let src = ['images/alcohol.jpg','images/detegent.jpg','images/fashion.jpg','images/home.jpg','images/phone.jpg','images/shoe.jpg'];
+
+// //constants
+// const img = document.createElement('img');
+// const dropdown = document.getElementsByClassName('dropdown');
+
+// setInterval(function(){
+//     img.setAttribute('src',src[Math.floor(Math.random()*src.length)])
+//     document.querySelector('.categoryresults').appendChild(img);
+// },5000)
+
